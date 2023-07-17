315807806d2be1583080073c52947090dda3d780
Abdelrahman Ahmed <73317105+abdelrahman252@users.noreply.github.com>
2023-07-17 23:38:01 +0300

    adding some desgin in projects page , and fixed nav bug but got another bug XDD

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,80 +1,105 @@
-import { useContext, useState } from 'react'
+import { useContext, useState } from "react";
 
-import { Col, Container, Ratio, Row } from 'react-bootstrap'
-import Nav from 'react-bootstrap/Nav'
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
+import { Col, Container, Ratio, Row } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-import "./projects.scss"
-import { AppContext } from '../../context/context/app_context'
+import "./projects.scss";
+import { AppContext } from "../../context/context/app_context";
 
 function Projects() {
+  const { appState, appDispatch } = useContext(AppContext);
 
-    const { appState, appDispatch } = useContext(AppContext)
+  const handleSelect = (key) =>
+    appDispatch({
+      type: "SET_CATEGORY",
+      payload: key,
+    });
+  const [showMore, setShowMore] = useState(false);
+  return (
+    <div id="projects" className="container project">
+      <div className="my-5 w-100">
+        <Container>
+          <Nav
+            variant="pills"
+            activeKey={appState.category}
+            onSelect={(key) => handleSelect(key)}
+          >
+            <Nav.Item>
+              <Nav.Link eventKey="uiux">UI/UX Design</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="mobile">Mobile Development</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="frontend">Frontend Development</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="backend">Backend Development</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="testing">Software Testing</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="ai">Artificial Intelligence</Nav.Link>
+            </Nav.Item>
+          </Nav>
 
-    const handleSelect = (key) => appDispatch({
-        type: 'SET_CATEGORY',
-        payload: key
-    })
-
-    return (
-        <div id="projects" className="container projects">
-            <div className='my-5 w-100'>
-                <Container>
-                    <Nav variant="pills" activeKey={appState.category} onSelect={key => handleSelect(key)}>
-                        <Nav.Item>
-                            <Nav.Link eventKey='uiux'>UI/UX Design</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="mobile">Mobile Development</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="frontend">Frontend Development</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="backend">Backend Development</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="testing">Software Testing</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="ai">Artificial Intelligence</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-
-                    <Row className='my-5'>
-                        {appState.projects[appState.category].map((project, index) =>
-                            <Col md={4} key={index}>
-                                <Card className='raduis-5'>{
-                                    project.image
-                                        ? <Card.Img className='raduis-5' variant="top" src={project.image} />
-                                        : <Ratio aspectRatio="16x9">
-                                            <embed
-                                                title="YouTube video player"
-                                                type="image/svg+xml"
-                                                src={project.embed}
-                                                frameborder="0"
-                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                                allowfullscreen />
-                                        </Ratio>    
-                                }
-                                    <Card.Body>
-                                        <Card.Title>{project.title}</Card.Title>
-                                        <Card.Text>
-                                            {project.description}
-                                        </Card.Text>
-                                        <Button variant="primary" href={project.url} target="_blank" rel="noopener noreferrer">
-                                            View Project
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        )}
-                    </Row>
-                </Container>
-            </div>
-        </div >
-    )
+          <Row className="my-5">
+            {appState.projects[appState.category].map((project, index) => (
+              <Col md={4} key={index} className="my-5">
+                <Card className="raduis-5">
+                  {project.image ? (
+                    <Card.Img
+                      className="raduis-5"
+                      variant="top"
+                      src={project.image}
+                    />
+                  ) : (
+                    <Ratio aspectRatio="16x9" className="raduis-5">
+                      <embed
+                        className="raduis-5"
+                        title="YouTube video player"
+                        type="image/svg+xml"
+                        src={project.embed}
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen
+                      />
+                    </Ratio>
+                  )}
+                  <Card.Body>
+                    <div>
+                      <Card.Title>{project.title}</Card.Title>
+                      <h6 className="desc">
+                        {showMore
+                          ? project.description
+                          : `${project.description.substring(0, 250)}`}
+                        <button
+                          className="showmoreBtn"
+                          onClick={() => setShowMore(!showMore)}
+                        >
+                          {showMore ? "Show less ▲" : "Show more ▼"}
+                        </button>
+                      </h6>
+                    </div>
+                    <a href={project.url} className="cta">
+                      <span>View Project</span>
+                      <svg width="13px" height="10px" viewBox="0 0 13 10">
+                        <path d="M1,5 L11,5"></path>
+                        <polyline points="8 1 12 5 8 9"></polyline>
+                      </svg>
+                    </a>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
--- src/components/shared/navbar/Header.jsx
+++ src/components/shared/navbar/Header.jsx
@@ -2,20 +2,25 @@ import { Navbar, Nav, Container } from "react-bootstrap"
 import "./navbar.scss"
 import home from "../../../assets/imgs/Ellipse.png"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+  const addClass = () => {
+    setIsActive((current) => !current);
+  };
   return (
     <Navbar className="header" expand="md">
-      <Container>
+      <Container className={isActive ? "bg" : ""}>
         <Navbar.Brand href="/">
           <img src={home} alt="home" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-white">
-          <span className="navbar-toggler-icon bg-white"></span>
+          <span className="navbar-toggler-icon bg-white" onClick={addClass}></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto p-2">
-            <Nav.Link href="#home">home</Nav.Link>
+            <Nav.Link href="/">home</Nav.Link>
             <Nav.Link href="#services">services</Nav.Link>
             <Nav.Link href="#skills">skills</Nav.Link>
             <Nav.Link href="#projects">projects</Nav.Link>
